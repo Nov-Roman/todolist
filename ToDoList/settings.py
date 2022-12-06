ab05930ae43596983dad9d2639e80f14b39bb37c
Nov-Roman <novosellov@gmail.com>
2022-12-07 02:28:56 +0500

    old

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -139,6 +139,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'core.User'
+
 
 SOCIAL_AUTH_JSONFIELD_ENABLED = True
 SOCIAL_AUTH_JSONFIELD_CUSTOM = 'django.db.models.JSONField'
