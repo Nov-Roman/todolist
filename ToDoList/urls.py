aefb129c715043eb1f16a54b1fac78b8639fe8eb
Nov-Roman <novosellov@gmail.com>
2022-12-07 22:43:04 +0500

    fix

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('core/', include('core.urls')),
-    path('oauth/', include('social_django.urls', namespace='social')),
-    path('goals/', include('goals.urls')),
+    path('admin/', admin.site.urls),
+    path('core/', include("core.urls")),
+    path('oauth/', include("social_django.urls", namespace="social")),
+    path('goals/', include("goals.urls")),
 ]
